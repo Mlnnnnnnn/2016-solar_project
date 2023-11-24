6c58c783f51f9a15e1704436253254471aa2e0b9
Yu-lianka <146694164+Yu-lianka@users.noreply.github.com>
2023-11-24 06:25:26 +0300

    Update solar_model.py
    calculate_force is fixed. It calculates force affects on objects

--- solar_model.py
+++ solar_model.py
@@ -19,8 +19,11 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        sin=(body.y - obj.y)/r
+        cos=(body.x - obj.x)/r
+        F=(gravitational_constant*body.m*obj.m)/(r**2)
+        body.Fx = F*cos 
+        body.Fy += F*sin
 
 
 def move_space_object(body, dt):
